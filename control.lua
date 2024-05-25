8ec9eca8bf3af6800727f3d4c9099766371f6cca
Luna <dev@lunardiver.com>
2024-05-25 09:31:28 +0200

    Added basic functionality

--- control.lua
+++ control.lua
@@ -1,1 +1,23 @@
-game.log(serpent.block(update_hidden_techs))
+local update_hidden_techs = function(force)
+    if settings.global['Factorissimo2-hide-recursion'] and settings.global['Factorissimo2-hide-recursion'].value then
+        force.technologies['factory-recursion-t1'].enabled = false
+        force.technologies['factory-recursion-t2'].enabled = false
+    elseif settings.global['Factorissimo2-hide-recursion-2'] and settings.global['Factorissimo2-hide-recursion-2'].value then
+        force.technologies['factory-recursion-t1'].enabled = true
+        force.technologies['factory-recursion-t2'].enabled = false
+    else
+        force.technologies['factory-recursion-t1'].enabled = true
+        force.technologies['factory-recursion-t2'].enabled = true
+    end
+end
+
+script.on_init(function()
+    for _, force in pairs(game.forces) do
+        update_hidden_techs(force)
+    end
+end)
+script.on_event(defines.events.on_research_finished, function()
+    for _, force in pairs(game.forces) do
+        update_hidden_techs(force)
+    end
+end)
